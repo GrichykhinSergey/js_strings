57fb62567a436e71abca252416244429a0259664
GrichykhinSergey <90179867+GrichykhinSergey@users.noreply.github.com>
2021-09-07 17:23:49 +0300

    Add files via upload

---
+++ output_sym.js
@@ -0,0 +1,5 @@
+let string = 'Я изучаю JavaScript';
+
+console.log(string[2], string[0], string[string.length - 1]);
+console.log(string.charAt(2), string.charAt(0), string.charAt(string.length - 1));
+console.log(string.slice(2, 3), string.slice(0, 1), string.slice(-1));
